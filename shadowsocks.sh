82aaf19c2d317c97471bf9e5152418bdc9805301
zhwill <33590311+zhwill@users.noreply.github.com>
2018-08-02 15:39:51 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,3 +1,20 @@
+#！/bin/bash
 hostnamectl set-hostname mycentos
+yum update -y
 yum install python-setuptools && easy_install pip
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+cat > /etc/shadowsocks.json  << EOF
+{
+    "server": "0.0.0.0",
+    "port_password": {
+        "8381": "password1",
+        "8382": "password2",
+        "8383": "password3",
+        "8384": "password4"
+    },
+    "timeout": 300,
+    "method": "aes-256-cfb"
+}
+EOF
+systemctl stop firewalld.service && systemctl disable firewalld.service
+nohub ssserver -c /etc/shadowsocks.json -d start
