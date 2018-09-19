6516a89119f8851e6ff807d3c6050ac0a52e3e30
will <524869004@qq.com>
2018-09-19 12:25:41 +0800

    1

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-hostnamectl set-hostname mycentos
-systemctl stop firewalld.service && systemctl disable firewalld.service
+#this is a shadowsocks shell
+hostnamectl set-hostname ssserver
 yum update -y
 yum install -y git epel-release libsodium python34-pip
 yum install python-setuptools && easy_install pip
@@ -18,4 +18,24 @@ cat > /etc/shadowsocks.json  << EOF
     "method": "xchacha20-ietf-poly1305"
 }
 EOF
+firewall-cmd --zone=public --add-port=8728-8731/tcp --permanent
+firewall-cmd reloa
 ssserver -c /etc/shadowsocks.json -d start
+#获取安装信息
+get_ip=$(ip a | grep ens33 | grep inet | awk '{print $2}' |sed 's/\/.*//')
+shadowsocks_port_pwd=$(sed -n '4,7p' /etc/shadowsocks.json | sed 's/^[ \t]*//g')
+shadowsockscipher=$(awk -F \" '/method/ {print $4}' /etc/shadowsocks.json)
+echo " " > serverinfo.txt
+echo -e "Shadowsocks server install completed!" >> serverinfo.txt
+echo " " >> serverinfo.txt
+echo -e "Your server ip： \n$get_ip" >> serverinfo.txt
+echo " " >> serverinfo.txt
+echo -e "Your Server Port and password：\n$shadowsocks_port_pwd" >> serverinfo.txt
+echo " " >> serverinfo.txt
+echo -e "Your Encryption Method: \n$shadowsockscipher" >> serverinfo.txt
+cat serverinfo.txt
+echo
+echo -e 'see this info again run the follow command \n# cat serverinfo.txt'
+echo
+
+
