628bf09a50d74a70c0ca3bd9ce73353f41959ca1
will <524869004@qq.com>
2018-09-19 13:33:03 +0800

    2

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #this is a shadowsocks shell
-hostnamectl set-hostname ssserver
-yum update -y
-yum install -y git epel-release libsodium python34-pip
-yum install python-setuptools && easy_install pip
-pip3 install  git+https://github.com/shadowsocks/shadowsocks.git@master
+hostnamectl set-hostname mycentos
+yum install -y python-setuptools && easy_install pip
+pip install shadowsocks
 cat > /etc/shadowsocks.json  << EOF
 {
     "server": "0.0.0.0",
@@ -15,17 +13,18 @@ cat > /etc/shadowsocks.json  << EOF
         "8731": "123456"
     },
     "timeout": 300,
-    "method": "xchacha20-ietf-poly1305"
+    "method": "aes-256-gcm"
 }
 EOF
 firewall-cmd --zone=public --add-port=8728-8731/tcp --permanent
-firewall-cmd reloa
+firewall-cmd reload
 ssserver -c /etc/shadowsocks.json -d start
 #获取安装信息
-get_ip=$(ip a | grep ens33 | grep inet | awk '{print $2}' |sed 's/\/.*//')
+get_ip=$(ip a | grep eth0 | grep inet | awk '{print $2}' |sed 's/\/.*//')
 shadowsocks_port_pwd=$(sed -n '4,7p' /etc/shadowsocks.json | sed 's/^[ \t]*//g')
 shadowsockscipher=$(awk -F \" '/method/ {print $4}' /etc/shadowsocks.json)
 echo " " > serverinfo.txt
+echo "##############################################################"
 echo -e "Shadowsocks server install completed!" >> serverinfo.txt
 echo " " >> serverinfo.txt
 echo -e "Your server ip： \n$get_ip" >> serverinfo.txt
@@ -37,5 +36,6 @@ cat serverinfo.txt
 echo
 echo -e 'see this info again run the follow command \n# cat serverinfo.txt'
 echo
+echo "##############################################################"
 
 
