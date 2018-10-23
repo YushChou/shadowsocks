9e32873a0abed29b7adf627d25080d7b7f29f7d0
will <524869004@qq.com>
2018-10-23 10:02:53 +0800

    20181023

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #this is a shadowsocks shell
-hostnamectl set-hostname mycentos
+hostnamectl set-hostname ss_server
 yum install -y epel-release git python-setuptools && easy_install pip
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 cat > /etc/shadowsocks.json  << EOF
@@ -24,18 +24,18 @@ ssserver -c /etc/shadowsocks.json -d start
 get_ip=$(ip a | grep eth0 | grep inet | awk '{print $2}' |sed 's/\/.*//')
 shadowsocks_port_pwd=$(sed -n '4,7p' /etc/shadowsocks.json | sed 's/^[ \t]*//g')
 shadowsockscipher=$(awk -F \" '/method/ {print $4}' /etc/shadowsocks.json)
-echo " " > serverinfo.txt
+echo " " > ss_info.txt
 echo "##############################################################"
-echo -e "Shadowsocks server install completed!" >> serverinfo.txt
-echo " " >> serverinfo.txt
-echo -e "Your server ip： \n$get_ip" >> serverinfo.txt
-echo " " >> serverinfo.txt
-echo -e "Your Server Port and password：\n$shadowsocks_port_pwd" >> serverinfo.txt
-echo " " >> serverinfo.txt
-echo -e "Your Encryption Method: \n$shadowsockscipher" >> serverinfo.txt
-cat serverinfo.txt
+echo -e "Shadowsocks server install completed!" >> ss_info.txt
+echo " " >> ss_info.txt
+echo -e "Your server ip： \n$get_ip" >> ss_info.txt
+echo " " >> ss_info.txt
+echo -e "Your Server Port and password：\n$shadowsocks_port_pwd" >> ss_info.txt
+echo " " >> ss_info.txt
+echo -e "Your Encryption Method: \n$shadowsockscipher" >> ss_info.txt
+cat ss_info.txt
 echo
-echo -e 'see this info again run the follow command \n# cat serverinfo.txt'
+echo -e 'see this info again run the follow command \n# cat ss_info.txt'
 echo
 echo "##############################################################"
 ssserver -c /etc/shadowsocks.json -d status
